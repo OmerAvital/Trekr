c1fe7cc658ca9424b1f180e5ef6caa01f4f9979b
OmerAvital <76176305+OmerAvital@users.noreply.github.com>
2022-03-28 14:19:21 -0700

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Omer Avital on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
